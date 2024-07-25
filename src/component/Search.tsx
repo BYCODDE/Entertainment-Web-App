14fae66ecd7e0753c379fb3ca8c2c81fd9c4b786
BYCODDE <anaraliev217@gmail.com>
2024-07-25 23:31:47 +0400

    feat:search component added!

---
+++ src/component/Search.tsx
@@ -0,0 +1,16 @@
+import SearchIcon from "/assets/icon-search.svg";
+
+export default function Search() {
+  return (
+    <div className="bg-custom-black flex  justify-between items-center w-screen p-[16px]">
+      <div className="flex gap-[16px] ">
+        <img src={SearchIcon} alt="" />
+        <input
+          className=" focus:outline-none  opacity-70 tracking-[0.5px]  bg-custom-black"
+          type="text"
+          placeholder="Search for movies or TV series "
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
 import Header from "../component/Header";
-
+import Search from "../component/Search";
 export default function Home() {
   return (
-    <div className="bg-custom-black flex justify-center items-center min-h-screen flex-col w-screen">
-      <Header>
-
-      </Header>
+    <div className="bg-custom-black flex justify-center items-center min-h-screen flex-col w-screen ">
+      <Header />
+      <Search />
     </div>
   );
 }
