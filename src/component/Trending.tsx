dacb19a40b1db1ad1ccb2bad25e75e081acdb00b
BYCODDE <anaraliev217@gmail.com>
2024-07-25 23:50:01 +0400

    feat:trending added!

---
+++ src/component/Trending.tsx
@@ -0,0 +1,13 @@
+import beyondEarth from "/assets/thumbnails/beyond-earth/trending/small.jpg";
+export default function Trending() {
+  return (
+    <div className="flex items-start  w-screen  flex-col p-[16px]">
+      <h3 className="text-[20px] tracking-[0.312px]">Trending</h3>
+      <img
+        className=" mt-[16px]  rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] "
+        src={beyondEarth}
+        alt="beyondEarth"
+      />
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Header from "../component/Header";
 import Search from "../component/Search";
+import Trending from "../component/Trending";
 export default function Home() {
   return (
     <div className="bg-custom-black flex justify-center items-center min-h-screen flex-col w-screen ">
       <Header />
       <Search />
+      <Trending />
     </div>
   );
 }
