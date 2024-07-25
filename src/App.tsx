caee244d2d8c388989b57aebb0f19e495ac04937
BYCODDE <anaraliev217@gmail.com>
2024-07-25 20:26:08 +0400

    feat:routes and first main pages and components created!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./Home";
+import Login from "./component/Login";
+import SignUp from "./component/SignUp";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/component/Login.tsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+  return (
+    <div>
+123123123
+    </div>
+  )
+}
