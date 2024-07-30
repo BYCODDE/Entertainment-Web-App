fc2253219c84a965d20a6fe20f3e833b6ace04bf
BYCODDE <anaraliev217@gmail.com>
2024-07-30 13:43:51 +0400

    style:overflow hidden error fixed!

--- src/component/Trending.tsx
+++ src/component/Trending.tsx
@@ -2,9 +2,9 @@ import beyondEarth from "/assets/thumbnails/beyond-earth/trending/small.jpg";
 import bottomGear from "/assets/thumbnails/bottom-gear/trending/small.jpg";
 export default function Trending() {
   return (
-    <div className="overflow-hidden">
+    <div className="">
       <h3 className="text-[20px] tracking-[0.312px] ml-[20px]">Trending</h3>
-      <div className="flex items-start  w-full pt-[0]  p-[16px]  gap-[16px]">
+      <div className="flex items-start  w-full pt-[0]  p-[16px]  gap-[16px] overflow-hidden">
         <img
           className=" mt-[16px]  rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] "
           src={beyondEarth}
