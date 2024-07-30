2a816e486d47430ec2ed517b7cf8ca02a59220a6
BYCODDE <anaraliev217@gmail.com>
2024-07-30 13:40:17 +0400

    style:hole component styles fixed!

--- src/component/Trending.tsx
+++ src/component/Trending.tsx
@@ -1,13 +1,21 @@
 import beyondEarth from "/assets/thumbnails/beyond-earth/trending/small.jpg";
+import bottomGear from "/assets/thumbnails/bottom-gear/trending/small.jpg";
 export default function Trending() {
   return (
-    <div className="flex items-start  w-full  flex-col p-[16px] overflow-hidden">
-      <h3 className="text-[20px] tracking-[0.312px]">Trending</h3>
-      <img
-        className=" mt-[16px]  rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] "
-        src={beyondEarth}
-        alt="beyondEarth"
-      />
+    <div className="overflow-hidden">
+      <h3 className="text-[20px] tracking-[0.312px] ml-[20px]">Trending</h3>
+      <div className="flex items-start  w-full pt-[0]  p-[16px]  gap-[16px]">
+        <img
+          className=" mt-[16px]  rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] "
+          src={beyondEarth}
+          alt="beyondEarth"
+        />
+        <img
+          className=" mt-[16px]  rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] "
+          src={bottomGear}
+          alt="bottomGear"
+        />
+      </div>
     </div>
   );
 }
