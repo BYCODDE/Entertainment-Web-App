483898af1b25ebe7bf9090992d7591a0d28f9e49
BYCODDE <anaraliev217@gmail.com>
2024-07-31 14:49:28 +0400

    style:working on bookmark svgs!

--- src/component/Trending.tsx
+++ src/component/Trending.tsx
@@ -1,5 +1,5 @@
 import movieIcon from "/assets/icon-nav-movies.svg";
-
+import bookMark from "/assets/icon-bookmark-empty.svg";
 export default function Trending() {
   return (
     <div className="p-4 w-full h-[50%]">
@@ -11,7 +11,7 @@ export default function Trending() {
       </h3>
       <div className="flex  gap-[16px] w-full h-full">
         <div
-          className=" mt-[16px] rounded-[8px] w-full h-full p-[8px] max-w-[240px] max-h-[140px]"
+          className=" mt-[16px] rounded-[8px] w-full h-full p-[8px] max-w-[240px] max-h-[140px] relative"
           style={{
             backgroundImage:
               "url(/assets/thumbnails/beyond-earth/trending/small.jpg)",
@@ -19,7 +19,10 @@ export default function Trending() {
             backgroundPosition: "center",
           }}
         >
-          <div className="opacity-50 flex  text-white items-center justify-between mt-[50%] ">
+          <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] ">
+            <img src={bookMark} alt="bookMark" />
+          </div>
+          <div className="opacity-50 flex  text-white items-center justify-between mt-[60px] ">
             2019{" "}
             <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
             <img
@@ -34,14 +37,31 @@ export default function Trending() {
           <span className="text-[18px] tracking-[0.312px]">Beyond Earth</span>
         </div>
         <div
-          className=" mt-[16px] rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] "
+          className=" mt-[16px] rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] p-[8px] relative "
           style={{
             backgroundImage:
               "url(/assets/thumbnails/bottom-gear/trending/small.jpg)",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-        ></div>
+        >
+          <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] ">
+            <img src={bookMark} alt="bookMark" />
+          </div>
+          <div className="opacity-50 flex  text-white items-center justify-between mt-[60px] ">
+            2021{" "}
+            <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+            <img
+              src={movieIcon}
+              alt="movieIcon"
+              className="filter-custom-filter"
+            />
+            Movie
+            <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+            PG
+          </div>
+          <span className="text-[18px] tracking-[0.312px]">Bottom Gear</span>
+        </div>
       </div>
     </div>
   );
