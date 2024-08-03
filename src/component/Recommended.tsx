845b180b47592fb41ebb43c9808e145b455e8911
BYCODDE <anaraliev217@gmail.com>
2024-08-03 23:01:43 +0400

    style:adding new movies for now!

--- src/component/Recommended.tsx
+++ src/component/Recommended.tsx
@@ -5,6 +5,11 @@ import nolandBeyond from "/assets/thumbnails/no-land-beyond/regular/small.jpg";
 import bookMark from "/assets/icon-bookmark-empty.svg";
 import movieIcon from "/assets/icon-nav-movies.svg";
 import tvIcon from "/assets/icon-nav-tv-series.svg";
+import duringtheHunt from "/assets/thumbnails/during-the-hunt/regular/small.jpg";
+import sameAnswer2 from "/assets/thumbnails/same-answer-2/regular/small.jpg";
+import theRockies from "/assets/thumbnails/the-rockies/regular/small.jpg";
+import bookMarkFull from "/assets/icon-bookmark-full.svg";
+
 export default function Recommended() {
   return (
     <div className="flex   w-full h-full  flex-col p-[16px] overflow-hidden ">
@@ -61,6 +66,79 @@ export default function Recommended() {
             <span className="text-[14px] tracking-[0.312px]">
               Earth’s Untouched
             </span>
+          </div>
+          <div className="relative">
+            <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] top-[3%] ">
+              <img src={bookMark} alt="bookMark" />
+            </div>
+            <img
+              src={duringtheHunt}
+              alt="duringtheHunt"
+              className="rounded-[8px] max-h-[110px] max-w-[164px]"
+            />
+            <div className="opacity-50 flex  text-white items-center justify-start gap-[6px] mt-[10px] text-[11px] ">
+              2016{" "}
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+              <img
+                src={tvIcon}
+                alt="tvIcon"
+                className="filter-custom-filter w-[11px] h-[11
+                px]"
+              />
+              TV Series
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>PG
+            </div>
+            <span className="text-[14px] tracking-[0.312px]">
+              During the Hunt
+            </span>
+          </div>
+          <div className="relative">
+            <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] top-[3%] ">
+              <img src={bookMark} alt="bookMark" />
+            </div>
+            <img
+              src={sameAnswer2}
+              alt="sameAnswer2"
+              className="rounded-[8px] max-h-[110px] max-w-[164px]"
+            />
+            <div className="opacity-50 flex  text-white items-center justify-start gap-[6px] mt-[10px] text-[11px] ">
+              2017{" "}
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+              <img
+                src={movieIcon}
+                alt="movieIcon"
+                className="filter-custom-filter w-[11px] h-[11
+                px]"
+              />
+              Movie
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>E
+            </div>
+            <span className="text-[14px] tracking-[0.312px]">
+              Same Answer II
+            </span>
+          </div>
+          <div className="relative">
+            <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] top-[3%] ">
+              <img src={bookMarkFull} alt="bookMarkFull" />
+            </div>
+            <img
+              src={theRockies}
+              alt="theRockies"
+              className="rounded-[8px] max-h-[110px] max-w-[164px]"
+            />
+            <div className="opacity-50 flex  text-white items-center justify-start gap-[6px] mt-[10px] text-[11px] ">
+              2015{" "}
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+              <img
+                src={movieIcon}
+                alt="movieIcon"
+                className="filter-custom-filter w-[11px] h-[11
+                px]"
+              />
+              TV Series
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>E
+            </div>
+            <span className="text-[14px] tracking-[0.312px]">The Rockies</span>
           </div>
         </div>
         <div className="flex flex-col gap-[15px]">
