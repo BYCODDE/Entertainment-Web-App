3562665eb154d07b08ab511d082f5a69fbab2af4
BYCODDE <anaraliev217@gmail.com>
2024-08-05 21:00:05 +0400

    feat:fetch logic added!

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./component/Login";
 import SignUp from "./component/SignUp";
 import Home from "./pages/Home";
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("data.json");
+        if (!response.ok) {
+          throw Error("Could not fetch the data");
+        }
+        const jsonData = await response.json();
+        console.log(jsonData.map((item: string) => item.title));
+
+        setData(jsonData);
+        // setIsLoading(true);
+      } catch (err) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
