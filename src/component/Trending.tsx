e29a967f3bd8431539b164b87afdae08d4d2c1f4
BYCODDE <anaraliev217@gmail.com>
2024-08-09 21:25:33 +0400

    refactor:working on maping trending movies!

--- src/component/Trending.tsx
+++ src/component/Trending.tsx
@@ -16,37 +16,33 @@ console.log(data);
         Trending
       </h3>
       <div className="flex  gap-[16px] w-full h-full">
-        {/* {
-          data?.trending?.map((item)=> (
-        <div
-          className=" mt-[16px] rounded-[8px] w-full h-full p-[8px] max-w-[240px] max-h-[140px] relative"
-          style={{
-            backgroundImage:
-              "url(/assets/thumbnails/beyond-earth/trending/small.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] ">
-            <img src={bookMark} alt="bookMark" />
+        {data?.map((item) => (item.isTrending&&
+          <div
+            className=" mt-[16px] rounded-[8px] w-full h-full p-[8px] max-w-[240px] max-h-[140px] relative"
+            style={{
+              backgroundImage: `url(${item?.thumbnail.trending?.small})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="bg-[#10141E] bg-opacity-[0.500647] w-[32px] h-[32px] rounded-[50%] flex items-center justify-center absolute right-[3%] ">
+              <img src={bookMark} alt="bookMark" />
+            </div>
+            <div className="opacity-50 flex  text-white items-center justify-between mt-[60px] ">
+              2019{" "}
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+              <img
+                src={movieIcon}
+                alt="movieIcon"
+                className="filter-custom-filter"
+              />
+              Movie
+              <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
+              PG
+            </div>
+            <span className="text-[18px] tracking-[0.312px]">Beyond Earth</span>
           </div>
-          <div className="opacity-50 flex  text-white items-center justify-between mt-[60px] ">
-            2019{" "}
-            <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
-            <img
-              src={movieIcon}
-              alt="movieIcon"
-              className="filter-custom-filter"
-            />
-            Movie
-            <div className="w-[3px] h-[3px] rounded-[50%] bg-[#FFF] "></div>
-            PG
-          </div>
-          <span className="text-[18px] tracking-[0.312px]">Beyond Earth</span>
-        </div>
-
-          ))
-        } */}
+        ))}
         {/* <div
           className=" mt-[16px] rounded-[8px] w-full h-full max-w-[240px] max-h-[140px] p-[8px] relative "
           style={{
